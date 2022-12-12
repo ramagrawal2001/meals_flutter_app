785a1cc09b32d4cadaf4d19f1594bb45bc4d74b7
ramagrawal2001 <ramagrawal0610@gmail.com>
2022-12-12 20:39:36 +0530

    Added Meals idem and detail pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/category_meals_screen.dart';
 
-import './categories_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/categories_screen.dart';
+import './screens/meal_detail_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
       home: CategoriesScreen(),
       routes: {
         CategoryMealsScreen.routeName: ((context) => CategoryMealsScreen()),
+        MealDetailScreen.routeName:(context) => MealDetailScreen(),
       },
     );
   }
